4c438e7299453015bda952be8dc48c2f34de89a8
StefanoMDeveloper <stenmarti02@gmail.com>
2022-02-10 16:46:07 +0100

    aggiunto footer

---
+++ src/components/commons/FooterMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div  class="menu">
+    <ul>
+      <li class="text-orange">home</li>
+      <li>Who We Are</li>
+      <li>What we Do</li>
+      <li>Where We Work</li>
+      <li>Careers</li>
+      <li>News</li>
+      <li>Get in touch now</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavMenu",
+};
+</script>
+
+<style scoped lang="scss">
+@import '../../assets/style/partials/variables.scss';
+ul {
+  list-style-type: none;
+  padding: 10px 0px;
+  li {
+    display: inline;
+    margin: 0 18px;
+    text-transform: capitalize;
+    font-size: 14px;
+  }
+}
+
+.menu{
+    padding: 40px 0;
+}
+
+</style>
--- src/components/macro/Footer.vue
+++ src/components/macro/Footer.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="sezione">
-    
+    <div class="my-container text-center">
+      <img src="../../assets/img/logo_seo_1x.png" alt="" />
+      <FooterMenu />
+
+      <p class="crediti">
+        @ Copyright 2012-2022 | Avada Theme by <span>StefanoMDeveloper</span> |
+        All Rigths Not Reserved | Not Powered By <span>Wordpress</span>
+      </p>
+      <i class="fas fa-brands fa-facebook-f"></i>
+      <i class="fas fa-brands fa-instagram"></i>
+      <i class="fas fa-brands fa-twitter"></i>
+      <i class="fas fa-brands fa-youtube"></i>
+    </div>
   </div>
 </template>
 
 <script>
+import FooterMenu from "../commons/FooterMenu.vue";
 export default {
-    name: "Footer"
-}
+  name: "Footer",
+  components: {
+    FooterMenu,
+  },
+};
 </script>
 
 <style scoped lang="scss">
+@import "../../assets/style/partials/variables.scss";
+.sezione {
+  background-color: white;
+  padding: 50px 0;
+}
 
+span {
+  color: black;
+}
+
+.crediti, svg {
+  color: grey;
+}
+
+svg{
+  font-size: 30px;
+  padding: 10px 20px;
+}
 </style>
