ee9019e877fdf9f14643312f0c5165930fc073c9
StefanoMDeveloper <stenmarti02@gmail.com>
2022-02-10 10:25:59 +0100

    sistemato nav menu

--- src/components/commons/NavMenu.vue
+++ src/components/commons/NavMenu.vue
@@ -5,7 +5,7 @@
       <li>Who We Are</li>
       <li>What we Do</li>
       <li>Where We Work</li>
-      <li>Careers <span>applay</span></li>
+      <li>Careers <span class="apply">apply</span></li>
       <li>News</li>
       <li class="btn rounded-pill orange-button">Get in touch now</li>
     </ul>
@@ -19,16 +19,27 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../../assets/style/partials/variables.scss';
 ul {
   list-style-type: none;
   padding: 10px 0px;
   float: right;
   li {
     display: inline;
-    margin: 0 15px;
+    margin: 0 12px;
     text-transform: capitalize;
     font-size: 14px;
     color: white;
   }
+}
+
+.apply{
+  padding: 5px;
+  border: 1px solid $yellow-orange;
+  color: $yellow-orange;
+  font-size: 12px;
+  margin-left: 5px;
+  border-radius: 7px;
+  text-transform: uppercase;
 }
 </style>
